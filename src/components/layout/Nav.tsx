cb0286f3929fcd78b04308a885a79a6fdc69a721
Jonas Deipenbrock <jonas.deipenbrock@gmx.de>
2022-07-10 19:51:43 +0200

    adds daisy ui and new navbar
    Signed-off-by: Jonas Deipenbrock <jonas.deipenbrock@gmx.de>

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -4,12 +4,65 @@ import NavBar from "./NavBar";
 const Nav = () => {
     return (
         <>
-            <nav className="flex justify-between items-center p-4 bg-gray-800">
-                <Link href="/">
-                    <a className="text-white text-2xl font-bold">GrowX</a>
-                </Link>
-                <NavBar />
-            </nav>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <Link href="/timetable">
+                                    <a>Timetable</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/timetable">
+                                    <a>Item 2</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <Link href="/">
+                        <a className="btn btn-ghost normal-case text-xl">
+                            Grow X
+                        </a>
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        <li>
+                            <Link href="/timetable">
+                                <a>Timetable</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/timetable">
+                                <a>Item 2</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <Link href="/">
+                        <a className="btn">Participate</a>
+                    </Link>
+                </div>
+            </div>
         </>
     );
 };
