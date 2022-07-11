126cc0defac30e8c5fdafa701ca13352c13fcd6d
Niklas <niklas.lutze@student.kit.edu>
2022-07-11 17:59:44 +0200

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="footer footer-center p-4 bg-neutral text-white decoration-white absolute bottom-0">
+        <footer className="footer footer-center p-4 bg-neutral text-white decoration-white">
             <div className="grid grid-flow-col gap-4">
                 <Link href="faq"><a className="link link-hover">FAQ</a></Link>
                 <Link href=""><a className="link link-hover">Contact</a></Link>
