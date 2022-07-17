ce2a9491265af682c4fe567dc3432caee122542b
Niklas <niklas.lutze@student.kit.edu>
2022-07-17 21:25:43 +0200

    use custom layout if available

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,26 @@
 import '../styles/globals.css'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import Layout from 'components/layouts/Layout'
+import Layout from '@/components/layouts/Layout'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) =>
+    <Layout>
+      {page}
+    </Layout>
+  )
+
+  return getLayout(<Component {...pageProps} />)
+}
+
+
