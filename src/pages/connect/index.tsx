2454dfaf094840727375d35d2d12636ca70fb49f
Niklas <niklas.lutze@student.kit.edu>
2022-08-04 05:40:04 +0200

    list profiles for tsting purposes

--- src/pages/connect/index.tsx
+++ src/pages/connect/index.tsx
@@ -1,15 +1,66 @@
 import ConnectLayout from '@/components/layouts/ConnectLayout';
 import PageLink from '@/components/nav/PageLink';
-import { Heading, VStack } from '@chakra-ui/react';
+import {
+    Box,
+    Heading,
+    HStack,
+    SimpleGrid,
+    Text,
+    Tag,
+    Flex,
+} from '@chakra-ui/react';
 import { withPageAuth } from '@supabase/auth-helpers-nextjs';
-import { NextPageWithLayout } from 'types';
+import { useUser } from '@supabase/auth-helpers-react';
+import useProfiles from 'hooks/useProfiles';
+import { useEffect } from 'react';
+import { NextPageWithLayout, ProfileDto } from 'types';
+
+function ProfileCard({ profile }: { profile: ProfileDto }) {
+    return (
+        <Flex bg="whiteAlpha.200" p={4} borderRadius={4} flexDir="column">
+            <Heading as="h3" size="md">
+                {profile.first_name} {profile.last_name}
+            </Heading>
+            <Box flexGrow={1}>
+                <Text fontSize="sm" color="whiteAlpha.600">
+                    {profile.email}
+                </Text>
+                <Text fontSize="sm" color="whiteAlpha.600">
+                    {profile.phone}
+                </Text>
+            </Box>
+
+            <HStack mt={2}>
+                <Tag>Computer Science</Tag>
+                <Tag>Marketing</Tag>{' '}
+            </HStack>
+        </Flex>
+    );
+}
 
 const ConnectIndex: NextPageWithLayout = () => {
+    const {user} = useUser()
+    const { values: profiles, fetch: fetchProfiles } = useProfiles();
+    useEffect(() => {
+        fetchProfiles();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user]);
     return (
-        <VStack>
+        <Box>
             <Heading>GROWconnect</Heading>
             <PageLink href="/">Back to Landing Page</PageLink>
-        </VStack>
+
+            <Box mt={8}>
+                <Heading size="md" as="h3">
+                    Participants
+                </Heading>
+                <SimpleGrid columns={4} gap={4} mt={6}>
+                    {profiles.map((profile) => (
+                        <ProfileCard key={profile.user_id} profile={profile} />
+                    ))}
+                </SimpleGrid>
+            </Box>
+        </Box>
     );
 };
 
@@ -17,4 +68,6 @@ ConnectIndex.getLayout = (page) => <ConnectLayout>{page}</ConnectLayout>;
 
 export default ConnectIndex;
 
-export const getServerSideProps = withPageAuth({ redirectTo: '/connect/login' });
+export const getServerSideProps = withPageAuth({
+    redirectTo: '/connect/login',
+});
