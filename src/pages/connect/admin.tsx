5fd1937bb92be9d08e768afccb80e9ff172fd3de
Niklas <niklas.lutze@student.kit.edu>
2022-08-07 13:22:06 +0200

    add profile table to admin view

--- src/pages/connect/admin.tsx
+++ src/pages/connect/admin.tsx
@@ -1,19 +1,36 @@
 import FullTable from '@/components/FullTable';
 import ConnectLayout from '@/components/layouts/ConnectLayout';
-import { Heading, VStack } from '@chakra-ui/react';
-import { withPageAuth } from '@supabase/auth-helpers-nextjs';
-import { NextPageWithLayout } from 'types';
+import { Heading, useToast, VStack } from '@chakra-ui/react';
+import { supabaseClient, withPageAuth } from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState } from 'react';
+import { NextPageWithLayout, ProfileDto } from 'types';
 
 const AdminPage: NextPageWithLayout = () => {
+    const toast = useToast()
+    const [profiles, setProfiles] = useState<ProfileDto[]>([])
 
-    const mockProfiles = [
-        {firstName: "Peter", lastName: "Fox", user_id: "fdlkgjfdsglkj"}
-    ]
+    useEffect(() => {
+        (async () => {
+            const {data, error} = await supabaseClient.from<ProfileDto>('profiles')
+                .select('*')
+            if (error) {
+                toast({
+                    title: error.message,
+                    status: 'error',
+                    duration: 4000,
+                    isClosable: true,
+                })
+            }
+            if (data) {
+                setProfiles(data)
+            }
+        })()
+    }, [])
 
     return (
         <VStack maxW="container.lg" alignItems="stretch" gap={4}>
             <Heading>Admin</Heading>
-            <FullTable values={mockProfiles} idProp="user_id" heading="Profiles" />
+            <FullTable values={profiles} idProp="user_id" heading="Profiles" />
         </VStack>
     );
 };
