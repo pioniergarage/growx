9b1c320ae353f8e2e4a3ad853e6dea25b104e678
KanseiHara <kansei14@live.de>
2022-09-08 21:45:08 +0200

    Little content changes: "From idea to prototyp" into "From ideation to the launch""

--- src/components/landing/ShortTimeline.tsx
+++ src/components/landing/ShortTimeline.tsx
@@ -76,7 +76,7 @@ export default function ShortTimeline() {
     return (
         <VStack alignItems={{ base: 'center', md: 'start' }} spacing={4}>
             <Heading size="lg" textAlign="center">
-                From idea to prototype in 11 weeks
+                From the ideation to the launch in 11 weeks
             </Heading>
             <SimpleGrid columns={[1, 1, 1, 3]} gap={8} width="100%">
                 {events.map((event) => (
