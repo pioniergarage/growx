495c153a7ed6f514fd2f783c9c26b62c882898a7
Niklas Lutze <niklas.lutze@student.kit.edu>
2022-09-19 17:12:37 +0200

    add label to admin button

--- src/components/navigation/NavAdminMenu.tsx
+++ src/components/navigation/NavAdminMenu.tsx
@@ -1,21 +1,43 @@
 import {
+    Button,
     IconButton,
     Menu,
     MenuButton,
     MenuItem,
     MenuList,
+    Show,
+    useBreakpointValue,
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { FaCogs } from 'react-icons/fa';
+
+const ResponsiveMenuButton = () => {
+    const variant = useBreakpointValue({
+        base: undefined,
+        lg: <FaCogs />,
+    });
+    return (
+        <>
+            <Show above="md">
+                <Button variant="ghost" leftIcon={variant}>
+                    Admin
+                </Button>
+            </Show>
+            <Show below="md">
+                <IconButton
+                    aria-label="admin-menu-button"
+                    icon={<FaCogs />}
+                    variant="ghost"
+                />
+            </Show>
+        </>
+    );
+};
+
 const NavAdminMenu = () => {
     return (
         <Menu>
-            <MenuButton
-                as={IconButton}
-                aria-label="Admin"
-                icon={<FaCogs />}
-                variant="ghost"
-            />
+            <MenuButton><ResponsiveMenuButton /></MenuButton>
             <MenuList>
                 <Link href="/connect/admin/profiles">
                     <MenuItem>Profiles</MenuItem>
