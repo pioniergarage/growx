f82890df87b50b6fae909587cd6c8cd6ea0b182b
Niklas Lutze <niklas.lutze@student.kit.edu>
2022-09-19 17:41:55 +0200

    run lint

--- src/components/navigation/NavAdminMenu.tsx
+++ src/components/navigation/NavAdminMenu.tsx
@@ -5,30 +5,12 @@ import {
     MenuButton,
     MenuItem,
     MenuList,
-    Show,
     useBreakpointValue,
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { FaCogs } from 'react-icons/fa';
 
-const ResponsiveMenuButton = () => {
-    return (
-        <>
-            <Show above="md">
-                <Button variant="ghost" leftIcon={variant}>
-                    Admin
-                </Button>
-            </Show>
-            <Show below="md">
-                <IconButton
-                    aria-label="admin-menu-button"
-                    icon={<FaCogs />}
-                    variant="ghost"
-                />
-            </Show>
-        </>
-    );
-};
+
 
 const NavAdminMenu = () => {
     const variant = useBreakpointValue({
