d38dc91bdd3ef2c4d3383483299603ac61a30781
Niklas Lutze <niklas.lutze@student.kit.edu>
2022-10-04 23:22:43 +0200

    change view of date and time

--- src/components/events/GrowEventCard.tsx
+++ src/components/events/GrowEventCard.tsx
@@ -1,12 +1,4 @@
-import {
-    Box,
-    Button,
-    Flex,
-    Grid,
-    Heading,
-    Text,
-    useToast,
-} from '@chakra-ui/react';
+import { Button, Flex, Heading, Text, useToast } from '@chakra-ui/react';
 import { useUser } from '@supabase/auth-helpers-react';
 import {
     useRegisterUserToEvent,
@@ -28,17 +20,27 @@ type GrowEventCardProps = {
     registered: boolean;
 };
 
+/**
+ * Event element component for the grow connact page
+ * @param event the event contain all important information of the event.
+ * @param registered This parameter set the state for the registration
+ * @returns HTML code that display one event with the date, title, description, location, type and if the event is mandatory.
+ */
 const GrowEventCard: React.FC<GrowEventCardProps> = ({ event, registered }) => {
     const [registeredLocal, setRegisteredLocal] = useState(registered);
     const { registerUser } = useRegisterUserToEvent();
     const { unregisterUser } = useUnregisterUserFromEvent();
     const { user } = useUser();
     const toast = useToast();
-    const { day, month, over } = useMemo(() => {
+    const { day, month, time, over } = useMemo(() => {
         const day = String(event.date.getDate()).padStart(2, '0');
         const month = event.date.toLocaleString('en-US', { month: 'short' });
+        const time = event.date.toLocaleString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit',
+        });
         const over = new Date() > event.date;
-        return { day, month, over };
+        return { day, month, time, over };
     }, [event.date]);
 
     async function register() {
@@ -123,48 +125,36 @@ const GrowEventCard: React.FC<GrowEventCardProps> = ({ event, registered }) => {
     }
 
     return (
-        <Grid
-            gap={4}
-            gridTemplateColumns="3.5rem 1fr"
-            color={over ? 'gray.500' : 'inherit'}
-        >
-            <Box>
-                <Box as="span" verticalAlign="top">
-                    {month} {day}
-                </Box>
-            </Box>
-            <Flex flexDir="column">
-                <Flex gap={2}>
-                    <Heading size="md">{event.title}</Heading>
-                </Flex>
-                <Text color="gray.400">{event.description}</Text>
-                <Flex
-                    mt={1}
-                    flexWrap="wrap"
-                    gap={2}
-                    flexDir={{ base: 'column', sm: 'row' }}
-                    alignItems="start"
-                >
-                    {event.location ? (
-                        <EventTag icon={FaMapMarkerAlt}>
-                            {event.location}
-                        </EventTag>
-                    ) : undefined}
-                    {event.type === EventType.Hybrid ? (
-                        <EventTag icon={FaChromecast}>Hybrid</EventTag>
-                    ) : event.type === EventType.Online ? (
-                        <EventTag icon={FaCloud}>Online</EventTag>
-                    ) : event.type === EventType.Offline ? (
-                        <EventTag icon={FaBuilding}>Offline</EventTag>
-                    ) : undefined}
-                    {event.mandatory ? (
-                        <EventTag icon={FaExclamation}>Mandatory</EventTag>
-                    ) : undefined}
-                </Flex>
-
-                {!over ? actionButton : undefined}
+        <Flex flexDir="column" color={over ? 'gray.500' : 'inherit'}>
+            <Flex gap="2px" flexDir="column">
+                <Heading size="xs" color="primary">{month} {day}, {time}</Heading>
+                <Heading size="md">{event.title}</Heading>
             </Flex>
-        </Grid>
+            <Text color="gray.400">{event.description}</Text>
+            <Flex
+                mt={1}
+                flexWrap="wrap"
+                gap={2}
+                flexDir={{ base: 'column', sm: 'row' }}
+                alignItems="start"
+            >
+                {event.location ? (
+                    <EventTag icon={FaMapMarkerAlt}>{event.location}</EventTag>
+                ) : undefined}
+                {event.type === EventType.Hybrid ? (
+                    <EventTag icon={FaChromecast}>Hybrid</EventTag>
+                ) : event.type === EventType.Online ? (
+                    <EventTag icon={FaCloud}>Online</EventTag>
+                ) : event.type === EventType.Offline ? (
+                    <EventTag icon={FaBuilding}>Offline</EventTag>
+                ) : undefined}
+                {event.mandatory ? (
+                    <EventTag icon={FaExclamation}>Mandatory</EventTag>
+                ) : undefined}
+            </Flex>
+
+            {!over ? actionButton : undefined}
+        </Flex>
     );
 };
 
