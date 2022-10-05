0e2f6cca1dc2f2bbbce5b612a32df8523929a18c
Niklas Lutze <niklas.lutze@student.kit.edu>
2022-10-05 18:30:33 +0200

    show upcoming events

--- src/pages/connect/index.tsx
+++ src/pages/connect/index.tsx
@@ -1,52 +1,58 @@
-import PageLink from '@/components/navigation/PageLink';
-import {
-    Alert,
-    AlertTitle,
-    Box,
-    Flex,
-    Heading,
-    Text,
-    VStack,
-} from '@chakra-ui/react';
+import GrowEventCard from '@/components/events/GrowEventCard';
+import { Box, Flex, Heading, Text, VStack } from '@chakra-ui/react';
 import {
     getUser,
     supabaseServerClient,
     withPageAuth,
 } from '@supabase/auth-helpers-nextjs';
 import { definitions } from 'database/supabase';
+import { useGrowEvents } from 'hooks/event';
+import { useMemo } from 'react';
 
 interface ConnectIndexProps {
     profile: definitions['profiles'];
 }
 
 const ConnectIndex: React.FC<ConnectIndexProps> = ({ profile }) => {
+    const { events } = useGrowEvents();
+    const upcomingEvents = useMemo(() => {
+        if (events) {
+            const now = new Date();
+            return events
+                .filter((e) => e.date > now)
+                .slice(0, Math.min(events.length, 2));
+        } else {
+            return [];
+        }
+    }, [events]);
+
     return (
         <Flex wrap="wrap">
-            <VStack flexGrow={1} alignItems="start">
+            <VStack flexGrow={1} alignItems="start" gap={4}>
                 <Heading size="md">
                     <Text as="span" color="gray.400">
                         Welcome back,
                     </Text>{' '}
                     {profile.first_name}
                 </Heading>
-                <Box>
-                    <Alert status="warning">
-                        <AlertTitle>
-                            🚧🏗 Website under construction!
-                        </AlertTitle>
-                    </Alert>
-                    <Text>
-                        Nevertheless, feel free to look around, e.g. you can{' '}
-                        <PageLink href="/connect/profile" color="primary">
-                            adjust your profile
-                        </PageLink>{' '}
-                        or{' '}
-                        <PageLink href="/connect/temas" color="primary">
-                            browse teams
-                        </PageLink>
-                        .
-                    </Text>
-                </Box>
+                {upcomingEvents.length > 0 ? (
+                    <Box>
+                        <Heading
+                            mb={2}
+                            size="sm"
+                            color="gray.400"
+                            fontSize={12}
+                        >
+                            Upcoming Events
+                        </Heading>
+                        <VStack gap={8}>
+                            {upcomingEvents.map((event) => (
+                                <GrowEventCard key={event.id} event={event} />
+                            ))}
+                        </VStack>
+                    </Box>
+                ) : undefined}
+
             </VStack>
         </Flex>
     );
