c7b8a61e49e9c5997f6d424d4e7965f25cdec83d
Niklas <niklas.lutze@student.kit.edu>
2022-10-21 13:22:02 +0200

    fix countdown

--- src/modules/landing/Countdown.tsx
+++ src/modules/landing/Countdown.tsx
@@ -37,14 +37,16 @@ const Countdown: React.FC<CountdownProps> = ({
     };
 
     const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(to));
-    // eslint-disable-next-line no-undef
     const [timer, setTimer] = useState<NodeJS.Timer>();
 
     useEffect(() => {
-        if (!timer)
+        if (!timer) {
             setTimer(
-                setInterval(() => setTimeLeft(calculateTimeLeft(to)), 1000)
+                setInterval(() => {
+                    setTimeLeft(calculateTimeLeft(to));
+                }, 1000)
             );
+        }
         return () => clearInterval(timer);
     }, [to, timer]);
 
