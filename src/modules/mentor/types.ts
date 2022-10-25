f52e123f881dcce2bc7482f1312e4162e5bc4b63
Niklas <niklas.lutze@student.kit.edu>
2022-10-25 16:14:16 +0200

    remove PublicMentorProfile

--- src/modules/mentor/types.ts
+++ src/modules/mentor/types.ts
@@ -3,7 +3,3 @@ import { Profile } from "modules/profile/types";
 
 export type MentorAssignments = Record<number, Profile>;
 
-export type PublicMentorProfile = Pick<
-    Profile,
-    'userId' | 'firstName' | 'lastName' | 'avatar' | 'bio'
->;
