c3406591abc3f0b1368f9c9cb00d6987c60a26cf
Niklas Lutze <niklas.lutze@student.kit.edu>
2022-11-08 23:00:39 +0100

    add particles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, Divider } from '@chakra-ui/react';
+import { Box, BoxProps, Divider, Show } from '@chakra-ui/react';
 import { createClient } from '@supabase/supabase-js';
 import { motion } from 'framer-motion';
 
@@ -16,7 +16,68 @@ import LongTimeline from 'modules/landing/Timeline';
 import WaitingForBlock from 'modules/landing/WaitingForBlock';
 import { getSponsors } from 'modules/sponsor/api';
 import { Sponsor } from 'modules/sponsor/types';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import type { Engine } from 'tsparticles-engine';
+
+const ParticlesTest = () => {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+
+    return (
+        <Box
+            position="absolute"
+            top={0}
+            right={0}
+            left={0}
+            sx={{
+                '*': {
+                    height: 'max(50vh, 30rem)',
+                },
+            }}
+        >
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                options={{
+                    pauseOnOutsideViewport: true,
+                    fpsLimit: 120,
+                    particles: {
+                        color: {
+                            value: '#ffffff',
+                        },
+                        move: {
+                            direction: 'none',
+                            enable: true,
+                            random: true,
+                            speed: 1,
+                            straight: false,
+                        },
+                        number: {
+                            value: 40,
+                        },
+                        shape: {
+                            type: 'circle',
+                        },
+                        size: {
+                            value: { min: 0, max: 2 },
+                        },
+                    },
+                    detectRetina: true,
+                    fullScreen: {
+                        enable: false,
+                        zIndex: -1,
+                    },
+                }}
+            />
+        </Box>
+    );
+};
 
 const minutesToSeconds = (minutes: number) => minutes * 60;
 
@@ -56,6 +117,9 @@ const Home: React.FC<HomeProps> = ({
     const events = jsonEvents.map((e) => ({ ...e, date: new Date(e.date) }));
     return (
         <>
+            <Show above="lg">
+                <ParticlesTest />
+            </Show>
             <Section position="relative" minH="80vh">
                 <Box
                     as={motion.div}
