86f2e531684acb08e548c825935253fa97d5df19
Niklas Lutze <niklas.lutze@student.kit.edu>
2022-11-11 23:42:31 +0100

    style event page for mobile

--- src/pages/connect/events/[eventId].tsx
+++ src/pages/connect/events/[eventId].tsx
@@ -1,18 +1,31 @@
+import Card from '@/components/Card';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import {
+    Box,
     Breadcrumb,
     BreadcrumbItem,
     BreadcrumbLink,
-    Flex,
+    Button,
+    Grid,
     Heading,
     Text,
     VStack,
 } from '@chakra-ui/react';
 import { withPageAuth } from '@supabase/auth-helpers-nextjs';
+import { useUser } from '@supabase/auth-helpers-react';
 import { Database } from 'database/DatabaseDefition';
 import { mapEventDto } from 'modules/events/api';
-import EventTagList from 'modules/events/components/EventTagList';
+import { useRegistrationsOfUser } from 'modules/events/hooks';
+import { formatEventTime } from 'modules/events/utils';
 import Link from 'next/link';
+import { useMemo } from 'react';
+import {
+    FaCalendar,
+    FaChromecast,
+    FaClock,
+    FaMapMarkerAlt,
+    FaTicketAlt,
+} from 'react-icons/fa';
 
 const GrowEvent = ({
     eventRaw,
@@ -20,12 +33,22 @@ const GrowEvent = ({
     eventRaw: Database['public']['Tables']['events']['Row'];
 }) => {
     const event = mapEventDto(eventRaw);
+    const user = useUser();
+
+    const { registrations } = useRegistrationsOfUser(user?.id);
+    const registration = useMemo(
+        () => (registrations ?? []).find((r) => r.eventId == event.id),
+        [event.id, registrations]
+    );
+    const eventTimeFormatted = useMemo(
+        () => formatEventTime(event.date, event.duration),
+        [event.date, event.duration]
+    );
     return (
-        <VStack alignItems="start">
+        <VStack alignItems="stretch" gap={2}>
             <Breadcrumb
                 color="gray.500"
                 separator={<ChevronRightIcon color="gray.500" />}
-                mb={4}
             >
                 <BreadcrumbItem>
                     <Link href="/connect/events" passHref>
@@ -49,14 +72,53 @@ const GrowEvent = ({
                     </Link>
                 </BreadcrumbItem>
             </Breadcrumb>
-            <Heading size="sm" color="secondary">
-                {event.date.toLocaleString('de-DE')}
-            </Heading>
             <Heading size="md">{event.title}</Heading>
-            <Flex gap={4}>
-                <Text variant="info">{eventRaw.description}</Text>
-                <EventTagList event={event} transparent minW="7rem" />
-            </Flex>
+            <Text variant="info" fontSize="sm">
+                {eventRaw.description}
+            </Text>
+            <Card p={4}>
+                <Grid templateColumns="1.5rem 2fr auto" alignItems="center">
+                    <FaCalendar />
+                    <Text as="div" variant="info" fontWeight="light">
+                        Date
+                    </Text>
+                    <Box fontWeight="bold" textAlign="right">
+                        {event.date.toLocaleString('de-DE', {
+                            day: '2-digit',
+                            month: 'long',
+                        })}
+                    </Box>
+                    <FaClock />
+                    <Text as="div" variant="info" fontWeight="light">
+                        Time
+                    </Text>
+                    <Box fontWeight="bold" textAlign="right">
+                        {eventTimeFormatted}
+                    </Box>
+                    <FaMapMarkerAlt />
+                    <Text as="div" variant="info" fontWeight="light">
+                        Location
+                    </Text>
+                    <Box fontWeight="bold" textAlign="right">
+                        {event.location}
+                    </Box>
+                    <FaChromecast />
+                    <Text as="div" variant="info" fontWeight="light">
+                        Seats online left
+                    </Text>
+                    <Box fontWeight="bold" textAlign="right">
+                        ∞
+                    </Box>
+                    <FaTicketAlt />
+                    <Text as="div" variant="info" fontWeight="light">
+                        Seats in presence left
+                    </Text>
+                    <Box fontWeight="bold" textAlign="right">
+                        23
+                    </Box>
+                </Grid>
+            </Card>
+            <Button>Sign up</Button>
         </VStack>
     );
 };
