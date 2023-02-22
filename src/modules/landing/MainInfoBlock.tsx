7a7f503ab33221e6ee9e6cbecf2ee481c18c6ad5
Niklas Lutze <niklas.lutze@student.kit.edu>
2023-02-22 21:33:04 +0100

    remove final info

--- src/modules/landing/MainInfoBlock.tsx
+++ src/modules/landing/MainInfoBlock.tsx
@@ -1,16 +1,12 @@
 import {
     Box,
-    Button,
     Flex,
     Grid,
     GridItem,
     Heading,
-    Icon,
     Show,
     Text,
 } from '@chakra-ui/react';
-import Link from 'next/link';
-import { FaClock, FaMapMarkerAlt } from 'react-icons/fa';
 import AnimatedLogo from './AnimatedLogo';
 
 function Fact({ amount, title }: { amount: string; title: string }) {
@@ -34,45 +30,20 @@ export default function MainInfoBlock() {
             columnGap={6}
             rowGap={12}
         >
-            <GridItem colSpan={{ base: 1, lg: 2 }} placeSelf="stretch">
-                <Flex
-                    alignItems="center"
-                    flexDir="column"
-                    className="rounded px-3 py-5 border-2"
-                >
-                    <Heading
-                        className="neon-text"
-                        fontSize={{ base: '2.5rem', md: '4rem', lg: '5rem' }}
-                    >
-                        GROW X FINAL
-                    </Heading>
-                    <Text lineHeight="1.2">
-                        An evening full of innovative pitches, disruptive
-                        start-ups, renowned speakers & a legendary afterparty!
-                    </Text>
-                    <Flex alignItems="center" mt={4}>
-                        <Icon as={FaClock} mr={2} />
-                        <Box fontSize="sm">Sat, 21 Jan, 7pm</Box>
-                    </Flex>
-                    <Flex alignItems="center">
-                        <Icon as={FaMapMarkerAlt} mr={2} />
-                        <Box fontSize="sm">
-                            Audimax, Straße am Forum, 76131 Karlsruhe
-                        </Box>
-                    </Flex>
-                    <a href="https://grow-x.eventbrite.de" className="pt-5">
-                        <Button size="lg" className="block p-2 bg-gray-200">
-                            Get Your Free Ticket
-                        </Button>
-                    </a>
-                </Flex>
-            </GridItem>
             <GridItem>
                 <Flex
                     flexDir="column"
                     gap={4}
                     align={{ base: 'center', md: 'start' }}
                 >
+                    <Heading
+                        className="neon-text"
+                        lineHeight={1.0}
+                        fontWeight="400"
+                        fontSize={{ base: '7rem', md: '10rem' }}
+                    >
+                        Let it Grow
+                    </Heading>
                     <Box>
                         <Heading size="lg">
                             Germany&apos;s Largest Student Founding Contest
@@ -91,8 +62,8 @@ export default function MainInfoBlock() {
                         w="100%"
                         mt={10}
                     >
-                        <Fact title="startups" amount="70+" />
-                        <Fact title="prizes" amount="> 20 000€" />
+                        <Fact title="startups" amount="50+" />
+                        <Fact title="prizes" amount="20 000€" />
                         <Fact title="workshops" amount="11" />
                     </Flex>
                 </Flex>
