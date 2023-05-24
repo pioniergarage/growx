6cab1f613d4cae3c51cc340882ffc7b2208cf70d
Gary Timeless <gary.timeless@gmx.de>
2023-05-24 14:25:33 +0200

    change headline to mark it is past (last)

--- src/modules/landing/Timeline.tsx
+++ src/modules/landing/Timeline.tsx
@@ -10,7 +10,7 @@ interface LongTimelineProps {
 const LongTimeline: React.FC<LongTimelineProps> = ({ events }) => {
     return (
         <VStack gap={4}>
-            <Heading>Timeline</Heading>
+            <Heading>Timeline 22/23 - LAST TIME</Heading>
             <VStack gap={4} alignItems="stretch" maxW="3xl">
                 {events.map((event) => (
                     <GrowEventCard key={event.id} event={event} />
